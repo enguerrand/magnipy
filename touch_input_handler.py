9e5a79aeaa1412301ac973ac6881fd658d0f1bf1
edr <github@rochefort.de>
2020-03-10 21:06:19 +0100

    Move ungrab of evdev devices to try/finally construct

--- touch_input_handler.py
+++ touch_input_handler.py
@@ -15,52 +15,58 @@ class TouchInputHandler:
         self.stop_requested = False
 
     def listen(self):
-        selector = selectors.DefaultSelector()
-        devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-        device_info = dict()
-        for device in devices:
-            capabilities = device.capabilities(verbose=False)
-            if is_touch_device(capabilities):
-                print(device.path, device.name, device.phys)
-                abs_info_abs_x = extract_absinfo(capabilities, ecodes.ABS_X)
-                abs_info_abs_y = extract_absinfo(capabilities, ecodes.ABS_Y)
-                if abs_info_abs_x is None or abs_info_abs_y is None:
-                    print("No abs_info found on x or y for device " + device.name)
-                    continue
-                device_info[device.path] = (abs_info_abs_x, abs_info_abs_y)
-                selector.register(device, selectors.EVENT_READ)
-                device.grab()
-                self.grabbed_devices.append(device)
+        try:
+            selector = selectors.DefaultSelector()
+            devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+            device_info = dict()
+            for device in devices:
+                capabilities = device.capabilities(verbose=False)
+                if is_touch_device(capabilities):
+                    print(device.path, device.name, device.phys)
+                    abs_info_abs_x = extract_absinfo(capabilities, ecodes.ABS_X)
+                    abs_info_abs_y = extract_absinfo(capabilities, ecodes.ABS_Y)
+                    if abs_info_abs_x is None or abs_info_abs_y is None:
+                        print("No abs_info found on x or y for device " + device.name)
+                        continue
+                    device_info[device.path] = (abs_info_abs_x, abs_info_abs_y)
+                    selector.register(device, selectors.EVENT_READ)
+                    self.grabbed_devices.append(device)
+                    # device.grab()
 
-        while not self.stop_requested:
-            for key, mask in selector.select(1):
-                device = key.fileobj
-                (abs_info_abs_x, abs_info_abs_y) = device_info[device.path]
-                x_size = abs_info_abs_x.max - abs_info_abs_x.min
-                y_size = abs_info_abs_y.max - abs_info_abs_y.min
-                for event in device.read():
-                    if event.type == ecodes.EV_ABS:
-                        if event.code == ecodes.ABS_X:
-                            if self.drag_start_x is not None:
-                                self.pan_zoom_state.pan_relative(delta_x=(event.value - self.drag_start_x) / x_size)
-                            self.drag_start_x = event.value
-                        if event.code == ecodes.ABS_Y:
-                            if self.drag_start_y is not None:
-                                self.pan_zoom_state.pan_relative(delta_y=(event.value - self.drag_start_y) / y_size)
-                            self.drag_start_y = event.value
-                        if event.code == ecodes.ABS_MT_TRACKING_ID:
-                            if event.value == -1:
-                                self.fingers_down = max(0, self.fingers_down - 1)
-                            else:
-                                self.fingers_down = self.fingers_down + 1
-                            print("fingers down: " + str(self.fingers_down))
-                            if self.fingers_down == 0:
-                                self.drag_start_x = None
-                                self.drag_start_y = None
+            while not self.stop_requested:
+                for ready in selector.select(1):
+                    if not ready:
+                        continue
+                    key, mask = ready
+                    device = key.fileobj
+                    (abs_info_abs_x, abs_info_abs_y) = device_info[device.path]
+                    x_size = abs_info_abs_x.max - abs_info_abs_x.min
+                    y_size = abs_info_abs_y.max - abs_info_abs_y.min
+                    for event in device.read():
+                        if event.type == ecodes.EV_ABS:
+                            if event.code == ecodes.ABS_X:
+                                if self.drag_start_x is not None:
+                                    self.pan_zoom_state.pan_relative(delta_x=(event.value - self.drag_start_x) / x_size)
+                                self.drag_start_x = event.value
+                            if event.code == ecodes.ABS_Y:
+                                if self.drag_start_y is not None:
+                                    self.pan_zoom_state.pan_relative(delta_y=(event.value - self.drag_start_y) / y_size)
+                                self.drag_start_y = event.value
+                            if event.code == ecodes.ABS_MT_TRACKING_ID:
+                                if event.value == -1:
+                                    self.fingers_down = max(0, self.fingers_down - 1)
+                                else:
+                                    self.fingers_down = self.fingers_down + 1
+                                print("fingers down: " + str(self.fingers_down))
+                                if self.fingers_down == 0:
+                                    self.drag_start_x = None
+                                    self.drag_start_y = None
+
+        finally:
+            for dev in self.grabbed_devices:
+                dev.ungrab()
 
     def stop(self):
-        for dev in self.grabbed_devices:
-            dev.ungrab()
         self.stop_requested = True
 
 
